4eacec4cd16062544dcf19ad181cbc3879bdbbea
geon2419 <geonhyeoklee.kr@gmail.com>
2026-01-21 11:56:52 +0900

    feat: add event handling; refactor Hazel project structure
    - Updated project files for Hazel and Sandbox to include new configurations (Dist).
    - Introduced logging functionality with spdlog for better debugging and information tracking.
    - Added event system with application events (WindowResize, WindowClose, etc.) to handle application state changes.
    - Refactored Application class to utilize new event system and logging.
    - Created new Sandbox application to demonstrate usage of Hazel framework.
    - Cleaned up project filters and included necessary source files for compilation.
    - Updated Premake script to manage project builds and dependencies more effectively.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
 namespace Hazel {
 	Application::Application()
 	{
@@ -11,6 +14,13 @@ namespace Hazel {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			HZ_TRACE(e.ToString());
+		}
+
+
 		while (true);
 	}
 }
