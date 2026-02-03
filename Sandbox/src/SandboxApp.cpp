4cfe89193ed4a3994df99b46d2b6394eb3140701
Geonhyuk Lee <geonhyeoklee.kr@gmail.com>
2026-02-03 20:52:14 +0900

    feat: add key and mouse button codes (#9)

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,4 +1,4 @@
-#include "Hazel.h"
+#include <Hazel.h>
 
 class ExampleLayer : public Hazel::Layer
 {
@@ -11,12 +11,24 @@ public:
 
 	void OnUpdate() override
 	{
-		//HZ_INFO("ExampleLayer::Update");
+		if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
+		{
+			HZ_TRACE("Tab key is pressed!");
+		}
+		
 	}
 
 	void OnEvent(Hazel::Event& event) override
 	{
-		//HZ_TRACE("{0}", event.ToString());
+		if (event.GetEventType() == Hazel::EventType::KeyPressed)
+		{
+			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+			if (e.GetKeyCode() == HZ_KEY_TAB)
+			{
+				HZ_TRACE("Tab key is pressed (event)!");
+			}
+			HZ_TRACE("{0}", (char)e.GetKeyCode());
+		}
 	}
 };
 
