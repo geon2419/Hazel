04719375797ed2a234891570eab8d5e1db3c8d93
Geonhyuk Lee <geonhyeoklee.kr@gmail.com>
2026-02-09 19:48:02 +0900

    feat: refactor application structure (#21)

--- Hazel/src/Hazel/Renderer/Shader.h
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -14,7 +14,7 @@ namespace Hazel {
 		void Bind() const;
 		void Unbind() const;
 
-		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix) const;
+		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
 	private:
 		uint32_t m_RendererID;
 	};
