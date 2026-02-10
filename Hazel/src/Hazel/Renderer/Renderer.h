8f13dfe3df2468b03542ee2355ccee9becd36ae6
Geonhyuk Lee <geonhyeoklee.kr@gmail.com>
2026-02-10 22:32:57 +0900

    feat: support per-object transforms in Renderer

--- Hazel/src/Hazel/Renderer/Renderer.h
+++ Hazel/src/Hazel/Renderer/Renderer.h
@@ -1,13 +1,14 @@
 #pragma once
 
-#include "RenderCommand.h"
+//#include "RenderCommand.h"
 #include "RendererAPI.h"
 #include "VertexArray.h"
 #include "OrthographicCamera.h"
 #include "Shader.h"
 
 #include <memory>
-#include <glm/ext/matrix_float4x4.hpp>
+//#include <glm/ext/matrix_float4x4.hpp>
+#include <glm/fwd.hpp>
 
 namespace Hazel {
 
@@ -17,7 +18,11 @@ namespace Hazel {
 		static void BeginScene(const OrthographicCamera& camera);
 		static void EndScene();
 
-		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
+		static void Submit(
+			const std::shared_ptr<Shader>& shader,
+			const std::shared_ptr<VertexArray>& vertexArray,
+			const glm::mat4& transform = glm::mat4(1.0f)
+		);
 
 		inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 	private:
