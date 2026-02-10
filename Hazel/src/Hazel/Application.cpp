09f1ebbb14e5d7dd508d886188367fe6c301c68b
Geonhyuk Lee <geonhyeoklee.kr@gmail.com>
2026-02-10 21:12:30 +0900

    feat: add Timestep class; integrate frame time into updates

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,6 +4,8 @@
 #include "Core.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include <GLFW/glfw3.h>
+
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -55,8 +57,12 @@ namespace Hazel {
 	{
 		while (m_Running)
 		{
+			float time = (float)glfwGetTime(); // TODO: Platform::GetTime()
+			Timestep timestep = time - m_LastFrameTime;
+			m_LastFrameTime = time;
+
 			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate();
+				layer->OnUpdate(timestep);
 			// TODO: you need to use another thread
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
