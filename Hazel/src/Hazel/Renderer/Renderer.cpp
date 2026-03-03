f4753f0e716e2d0962063a987ee300599975fd80
Geonhyeok Lee <geonhyeoklee.kr@gmail.com>
2026-03-03 21:45:25 +0900

    feat: implement window resize handling and viewport adjustment (#40)

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -16,6 +16,11 @@ void Renderer::Init()
     RenderCommand::Init();
 }
 
+void Renderer::OnWindowResize(uint32_t width, uint32_t height)
+{
+    RenderCommand::SetViewport(0, 0, width, height);
+}
+
 void Renderer::BeginScene(const OrthographicCamera& camera)
 {
     s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
